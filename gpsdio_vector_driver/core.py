08152f4ff1a088d79c9c962e32843e75fe9b7028
Kevin Wurster <kevin@skytruth.org>
2015-06-12 17:24:05 -0400

    Added some logging, better driver defaults, and better documentation.

--- gpsdio_vector_driver/core.py
+++ gpsdio_vector_driver/core.py
@@ -5,10 +5,15 @@ Vector driver implementation
 
 from collections import OrderedDict
 from copy import deepcopy
+import logging
 
 import fiona as fio
 from gpsdio.drivers import BaseDriver
 import six
+
+
+logging.basicConfig()
+logger = logging.getLogger('gpsdio-vector-driver')
 
 
 class Vector(BaseDriver):
@@ -26,7 +31,17 @@ class Vector(BaseDriver):
         speed:float:10.1
         heading:int:7
 
-    Uses Fiona: https://github.com/Toblerity/Fiona
+    gpsdio is pretty good about figuring out which drivers to use based on the
+    file extension, but that feature is not supported with this driver.
+
+    This driver in turn uses OGR, which supports many drivers, some of which
+    don't use connection strings instead of file paths, so the user is required
+    to specify the output driver if the default is not desired.
+
+    If unexpected results are encountered be sure to specify `driver`.
+
+    OGR: gdal.org
+    Fiona: github.com/Toblerity/Fiona
 
 
     Driver Options
@@ -52,23 +67,25 @@ class Vector(BaseDriver):
     """
 
     io_modes = 'w',
-    driver_name = 'vector'
-    extensions = ('shp', 'geojson')
+    driver_name = 'Vector'
+    extensions = ()
+    default_fields = OrderedDict((
+        ('mmsi', 'int:30'),
+        ('timestamp', 'str:40'),
+        ('course', 'float:12.1'),
+        ('speed', 'float:10.1'),
+        ('heading', 'int:7')
+    ))
 
     def __init__(self, f, mode='w', driver='ESRI Shapefile', line=None,
                  crs='EPSG:4326', fields=None):
 
-        properties = OrderedDict((
-            ('mmsi', 'int:30'),
-            ('timestamp', 'str:40'),
-            ('course', 'float:12.1'),
-            ('speed', 'float:10.1'),
-            ('heading', 'int:7')
-        ))
+        if not isinstance(f, six.string_types):
+            raise TypeError("File argument for vector driver must be a string.")
 
+        properties = self.default_fields.copy()
         if isinstance(fields, six.string_types):
             fields = fields.split(',')
-
         if isinstance(fields, (OrderedDict, dict)):
             properties.update(**fields)
         elif isinstance(fields, (list, tuple)):
@@ -96,6 +113,11 @@ class Vector(BaseDriver):
         self._line_meta = deepcopy(meta)
         self._line_meta['schema']['properties'] = {}
         self._line_meta['schema']['geometry'] = 'LineString'
+
+        logger.debug("Vector meta: %s", meta)
+        logger.debug("Line meta: %s", self._line_meta)
+        logger.debug("Line: %s", self._line)
+        logger.debug("BUH: %s", self.stream.schema['properties'])
 
     def write(self, msg):
         x = msg.get('lon')
